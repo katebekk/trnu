e376ed9625ea001439a26cfdcbf502f1ad584dc9
katebekk <85067@mail.ru>
2019-06-16 12:48:45 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    float  n, m,x1;
+    n = 289;
+    m = sqrt(n);
+    cout << m << endl;
+    float x2 = 10, x3 = 7;
+
+    x1 = x2 / x3;
+    cout << x1 << endl;
+    return 0;
+}
