3c6311e05e443cf408f1c3ab011053f533777d1e
katebekk <85067@mail.ru>
2019-06-16 12:56:06 +1000

    2 commit

--- main.cpp
+++ main.cpp
@@ -13,5 +13,15 @@ int main() {
 
     x1 = x2 / x3;
     cout << x1 << endl;
+    //изменения
+    float  n, m,x1;
+    n = 289;
+    m = sqrt(n);
+    cout << m << endl;
+    float x2 = 10, x3 = 7;
+
+    x1 = x2 / x3;
+    cout << x1 << endl;
+
     return 0;
 }
